0c2b9924109a08d31839b15df3e4b601f59e32cc
Paul-jambou <paul.jambou@etu.umontpellier.fr>
2022-03-08 20:44:23 +0100

    Ajout maven + class test

---
+++ src/test/java/FileUtilsTest.java
@@ -0,0 +1,51 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FileUtilsTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void generateIndex() {
+    }
+
+    @Test
+    public void createFile() {
+    }
+
+    @Test
+    public void readFile() {
+    }
+
+    @Test
+    public void getLines() {
+    }
+
+    @Test
+    public void getLine() {
+    }
+
+    @Test
+    public void getMateriel() {
+    }
+
+    @Test
+    public void copyDirectory() {
+    }
+
+    @Test
+    public void copy() {
+    }
+
+    @Test
+    public void copyFile() {
+    }
+
+    @Test
+    public void testCopy() {
+    }
+}
